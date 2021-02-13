dd684428e04ef28510fdb99e95d397f29388bb0e
Anna Przybysz <anna.przybysz212@gmail.com>
2021-02-13 11:34:48 +0100

    odczytanie ruchu gracza

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
+let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
@@ -12,3 +13,20 @@ else if(randomNumber == 3){
   computerMove = 'nożyce';
 }
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+if(playerInput == '2'){
+  playerMove = 'papier';
+}
+if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
